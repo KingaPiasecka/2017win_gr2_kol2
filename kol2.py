f5582a150bba095dc470d1ca2d9ddc2857999b1f
KingaPiasecka <piasecka.kingaa@gmail.com>
2017-11-16 17:08:56 +0100

    version_1

--- kol2.py
+++ kol2.py
@@ -13,3 +13,41 @@
 # If you have enough courage and time, try storing (reading/writing) 
 # data in text files (YAML, JSON).
 # If you have even more courage, try implementing user interface.
+
+class Student:
+	def __init__(self, student_name, studet_surname, student_id, list_of_scores, list_of_attendance):
+		self.student_id = student_id;
+		self.studnet_surname = studet_surname;
+		self.student_name = student_name;
+		list_of_scores = [];
+		list_of_attendance = [];
+		
+	def add_score(self, score):
+		self.list_of_scores.append(score)
+		
+	def get_average_of_scores(self):
+		return reduce(lambda x, y: x + y, self.list_of_scores) / float(len(l))
+		
+	def __repr__(self):
+		return "ID:" +  self.student_id
+
+class Subject:
+	def __init__(self, subject_name, list_of_students):
+		self.subject_name = subject_name;
+		self.list_of_students = list_of_students;
+	
+	def add_new_student(self, student_name, student_surname, student_id):
+		new_student = Student(student_name, student_surname, student_id, [], []);
+		self.list_of_students.append(new_student);
+		
+	def get_all_students(self):
+		print self.list_of_students
+		
+		
+if __name__=="__main__":
+	python_subject = Subject("Python in the enterprise", []);
+	python_subject.add_new_student("Jan", "Kowalski", "270000");
+	python_subject.get_all_students();
+	
+
+		
